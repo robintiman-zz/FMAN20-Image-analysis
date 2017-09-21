2d0ba5dade0b42f2186f5a45dc6805cde1e887fb
Robin Timan <robintiman@gmail.com>
2017-09-21 15:11:27 +0200

    Ass 2

---
+++ assignment-2/questions.m
@@ -0,0 +1,37 @@
+%% Q2
+f = [1 4 6 8 7 5 3];
+int = interp1(f, 1:7);
+figure
+plot(int)
+hold on
+scatter(1:7, f)
+
+%% Q3.1
+% Train
+c1 = [0.4003 0.3988 0.3998 0.3997 0.4010 0.3995 0.3991];
+c2 = [0.2554 0.3139 0.2627 0.3802 0.3287 0.3160 0.2924];
+c3 = [0.5632 0.7687 0.0524 0.7586 0.4243 0.5005 0.6769];
+xtrain = [c1(1:4) c2(1:4) c3(1:4)];
+Y = [ones(1,4) ones(1,4)*2 ones(1,4)*3];
+model = fitcknn(xtrain', Y');
+
+% Predict
+xtest = [c1(5:end) c2(5:end) c3(5:end)];
+label = predict(model, xtest')';
+
+%% Q3.2
+m = [0.4 0.3 0.5];
+std = [0.01 0.05 0.2];
+map = zeros(3, 7*3);
+for i=1:3
+    map(i,:) = normpdf([c1 c2 c3], m(i), std(i));
+end
+
+labels = zeros(1,7*3);
+for i=1:7*3
+    [~,label] = max(map(:,i));
+    labels(i) = label;
+end
+%% 
+
+
